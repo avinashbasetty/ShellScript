9300584f14b60ec9fca443493bf75661c2db1efe
avinashbasetty <avinashbasetty@gmail.com>
2019-03-19 16:26:16 +0530

    Update read_input.sh

--- read_input.sh
+++ read_input.sh
@@ -5,3 +5,13 @@ read -p 'enter user :' user_var
 read -sp 'password:' password_var
 echo "user name is $user_var"
 echo "password is $password_var"
+
+#Name with array
+echo "enter names"
+read -a names
+echo "names are ${names[0]} ${names[1]}"
+
+#read with no argument
+echo "enter name with no argument"
+read
+echo "default name is $REPLY"
